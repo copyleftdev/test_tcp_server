904b4b8a5c77b89ef76b9f06cbcee3eb34dc1afa
L337[c95df0a3]SIGMA <don@codetestcode.io>
2024-07-08 12:51:58 -0700

    Add files via upload

---
+++ target/debug/deps/simple_tcp_server-450d1fc4acadc420.d
@@ -0,0 +1,5 @@
+/home/ops/Projects/simple_tcp_server/target/debug/deps/simple_tcp_server-450d1fc4acadc420: src/main.rs
+
+/home/ops/Projects/simple_tcp_server/target/debug/deps/simple_tcp_server-450d1fc4acadc420.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/simple_tcp_server.d
@@ -0,0 +1,1 @@
+/home/ops/Projects/simple_tcp_server/target/debug/simple_tcp_server: /home/ops/Projects/simple_tcp_server/src/main.rs
